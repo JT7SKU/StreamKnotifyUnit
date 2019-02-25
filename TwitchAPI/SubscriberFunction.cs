3ac04651b3c74d37c8290951719be90d59863973
Jani Turunen <jani@janisku7.eu>
2019-02-25 20:24:29 +0200

    small update to function project

--- TwitchAPI/SubscriberFunction.cs
+++ TwitchAPI/SubscriberFunction.cs
@@ -12,7 +12,7 @@ namespace TwitchAPI
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
+        [FunctionName("SubscriberFunction")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
