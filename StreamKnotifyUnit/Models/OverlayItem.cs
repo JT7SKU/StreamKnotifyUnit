ece8a3b18685863647d0fdb40d9b57fafa2058b4
Jani Turunen <jani@janisku7.eu>
2019-04-02 01:17:02 +0300

    starting to build Overlay setup

---
+++ StreamKnotifyUnit/Models/OverlayItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreamKnotifyUnit.Models
+{
+    public class OverlayItem
+    {
+        public string Title { get; set; }
+        public string LinkerText { get; set; }
+    }
+}
